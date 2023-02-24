2857d15fd7d972df57593732ef0fe276203ebf63
jorpaz <giovanny.paz@outlook.com>
2023-02-24 10:22:33 -0600

    home config

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Header from '../components/Header';
 import '../styles/Home.css';
 
 const Home = () => {
 
 
     return (
-        <div className='container'>
+        <>
+        <div>
+        <Header />
         </div>
+            <div className='container'>
+                <div className='info-containers'>
+                    <h1>PRUEBA</h1>
+                </div>
+            </div>
+        </>
     );
-    };
+};
 
 export default Home;
