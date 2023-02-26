ae6c19e573791d3fb73a441c81e0670807702171
jorpaz <giovanny.paz@outlook.com>
2023-02-26 01:30:37 -0600

    css grid config

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -10,15 +10,26 @@ const Home = () => {
         <Header />
             <div className='container'>
                 <div className='info-containers-left'>
-                    <h1><a href="/">SERVICIOS</a></h1>
+                    <h1><a href="/">KRAFT & WELD</a></h1>
+                    <div className='image-back'>
+                        <img src="https://images.unsplash.com/photo-1558611997-dd5b20e08c71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80" alt="imagen" />
+                    </div>
                 </div>
                 <div className='info-containers-right'>
-                    <h1>PRUEBA2</h1>
+                    <h1>NUEVAS HERRAMIENTAS</h1>
+                    <h2>Lo mejor para una soldadura de calidad.</h2>
+                    <br />
+                    <p><a href="/nosotros">Conoce más acerca de nosotros.</a></p>
                 </div>
             </div>
             <div className='container-2'>
                 <div className='info-containers-left-2'>
-
+                    <div className='text-promo'>
+                        <h1>PRECIO ESPECIAL</h1>
+                    </div>
+                    <div className='text-promoP'>
+                        <p>Compralo ya</p>
+                    </div>
                 </div>
             </div>
         </>
