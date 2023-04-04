6c39cfa811f5376013cc135161a848d9ad59d12b
jorpaz <giovanny.paz@outlook.com>
2023-04-04 00:14:12 -0600

    services v2

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -10,7 +10,7 @@ const Services = () => {
             {<Header />}
             <main className="container-body">
                 <section className='card-1'>
-                    <h1>PRUEBA 1</h1>
+                    <h1>Servicio Uno</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, explicabo possimus corrupti id necessitatibus nesciunt voluptatem magni inventore sequi ad reprehenderit quasi ratione odio nobis a sint nam, quas cumque.</p>
                 </section>
             </main>
